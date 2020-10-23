154548d29f79b085f400340517e5070c9a1b1877
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-10-23 18:14:07 +0300

    Add Alert Controller

--- LoginApp/LogInViewController.swift
+++ LoginApp/LogInViewController.swift
@@ -28,13 +28,37 @@ class LogInViewController: UIViewController {
   
     // MARK: - IB Actions
     @IBAction func logInButtonPressed() {
+        guard
+            userNameTextField.text == userName,
+            passwordTextField.text == password
+                else {
+            showAlert(title: "Wrong login or password", message: "Please, enter correct login or password", textField: passwordTextField)
+            return
+        }
+        performSegue(withIdentifier: "logIn", sender: nil)
     }
     
     @IBAction func forgotUserNamePressed() {
+        showAlert(title: "Hey!", message: "User Name is \(userName)")
     }
     
     @IBAction func forgotPasswordPressed() {
+        showAlert(title: "Hey!", message: "Password is \(password)")
     }
     
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = nil
+        passwordTextField.text = nil
+    }
 }
 
+// MARK: - Alert Controller
+extension LogInViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in textField?.text = nil
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
